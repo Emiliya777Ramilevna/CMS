b9fea37f02210fdeb51eeda13004dc05bd539ee5
Emiliya Milevskaya <Emiliya777ramilevna@gmail.com>
2025-07-15 17:17:05 +0300

    hotfix/20-finish

--- js/index.js
+++ js/index.js
@@ -1,49 +1,44 @@
-// const buttonText = document.querySelector(".content-button__expand")
-// const text = document.querySelector(".content-article__text")
+const buttonText = document.querySelector(".content-button__expand")
+const text = document.querySelector(".content-article__text")
     
-// buttonText.addEventListener("click", function() { 
-//        text.classList.toggle("expanded-height-text");
-//         buttonText.classList.toggle("content-button__hidden");
-//     if (buttonText.textContent === "Читать далее") { 
-//         buttonText.textContent = "Скрыть";
-//     }
-//     else {
-//         buttonText.textContent = "Читать далее";
-//     }
-// })
+buttonText.addEventListener("click", function() { 
+       text.classList.toggle("expanded-height-text");
+        buttonText.classList.toggle("content-button__hidden");
+    if (buttonText.textContent === "Читать далее") { 
+        buttonText.textContent = "Скрыть";
+    }
+    else {
+        buttonText.textContent = "Читать далее";
+    }
+})
 
-// const buttonBrands = document.querySelector(".brands-button__expand")
-// const brandsList = document.querySelector(".brands-list")
+const buttonBrands = document.querySelector(".brands-button__expand")
+const brandsList = document.querySelector(".brands-list")
     
-// buttonBrands.addEventListener("click", function() { 
-//        brandsList.classList.toggle("expanded-height-brands");
-//         buttonBrands.classList.toggle("brands-button__hide");
-//     if (buttonBrands.textContent === "Показать все") { 
-//         buttonBrands.textContent = "Скрыть";
-//     }
-//     else {
-//         buttonBrands.textContent = "Показать все";
-//     }
-// })
+buttonBrands.addEventListener("click", function() { 
+       brandsList.classList.toggle("expanded-height-brands");
+        buttonBrands.classList.toggle("brands-button__hide");
+    if (buttonBrands.textContent === "Показать все") { 
+        buttonBrands.textContent = "Скрыть";
+    }
+    else {
+        buttonBrands.textContent = "Показать все";
+    }
+})
 
-// const buttonTechnique = document.querySelector(".technique-button__expand")
-// const techniqueList = document.querySelector(".technique-list")
+const buttonTechnique = document.querySelector(".technique-button__expand")
+const techniqueList = document.querySelector(".technique-list")
     
-// buttonTechnique.addEventListener("click", function() { 
-//        techniqueList.classList.toggle("expanded-height-technique");
-//         buttonTechnique.classList.toggle("technique-button__hide");
-//     if (buttonTechnique.textContent === "Показать все") { 
-//         buttonTechnique.textContent = "Скрыть";
-//     }
-//     else {
-//         buttonTechnique.textContent = "Показать все";
-//     }
-// })
-
-
-
-
-
+buttonTechnique.addEventListener("click", function() { 
+       techniqueList.classList.toggle("expanded-height-technique");
+        buttonTechnique.classList.toggle("technique-button__hide");
+    if (buttonTechnique.textContent === "Показать все") { 
+        buttonTechnique.textContent = "Скрыть";
+    }
+    else {
+        buttonTechnique.textContent = "Показать все";
+    }
+})
 
 // Пробная весрия на свайпер
 
@@ -72,19 +67,11 @@
 // window.addEventListener("resize", initOrDestroySwiper);
 
 
-
-
-
-
-
-
 const slider = document.querySelector('.swiper-container');
-
 let mySwiper;
 
 function mobileSlider() {
-    if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
-
+    if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
         mySwiper = new Swiper(slider, {
             loop: false,
             slideClass: 'swiper-slide',
@@ -92,89 +79,36 @@ function mobileSlider() {
                     el: '.swiper-pagination',
                     clickable: true,
                 },
-
                 simulateTouch: true,
-
                 touchRatio: 1,
-
                 touchAngle: 45,
-
                 grabCursor: true,
-
                 slideToClickedSlide: true,
-
                 slideToClickedSlide: true,
-
-
                 keyboard: {
-
                     enabled: true,
-
                     onlyInViewport: true,
-
                     pageUpDown: true,
                 },
-
-
                 mousewheel: {
-        
                     sensitivity: 1,
-
-        
                     eventsTarget: '.swiper-container',
                 },
-
-                breakpoints: {
-                    240: {
-                        slidesPerView: 0.8, 
-                    },
-
-                    280: {
-                        slidesPerView: 1,
-                    },
-
-                    320: {
-                        slidesPerView: 1.35,  
-                    },
-
-                    400: {
-                        slidesPerView: 1.7, 
-                    },
-        
-                    480: {
-                        slidesPerView: 2, 
-                    },
-
-                    600: {
-                        slidesPerView: 2.5, 
-                    }, 
-        
-                    755: {
-                        slidesPerView: 3,
-                    },
-
-                    768: {
-                        initialSlide: 0,
-                        spaceBetween: 0,
-                        enabled: false,
-                        },
-                },
+                slidesPerView: "auto",
                 centeredSlides: false,
                 watchOverflow: true,
                 spaceBetween: 16,
                 slidesOffsetBefore: 0,
                 slidesOffsetAfter: 0,
         });
-
         slider.dataset.mobile = 'true';
     }
-
-    if (window.innerWidth > 768) {
+    if (window.innerWidth >= 768) {
         slider.dataset.mobile = 'false';
-
         if (slider.classList.contains('slider-initialized')) {
-
             mySwiper.destroy();
+            swiperInstance.destroy(true, true);
+            swiperInstance = null;
         }
     }
 }
